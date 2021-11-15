ab239854c0fd9f40af7b7455f5a92275363dce8f
Dariush Habibpour <dariushhpg1@gmail.com>
2021-11-15 13:36:50 +0330

    Commit

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-src = "https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
-integrity = "sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
-crossorigin = ""
-
-
-
-
 //Map initialization
-var map = L.map('map').setView([35.7, 51.35], 12);
+var map = L.map('map').setView([35.6892, 51.3890], 12);
 
 //osm layer
 var osm = L.tileLayer('https://api.maptiler.com/maps/pastel/{z}/{x}/{y}.png?key=lzIzrp8GaiFvAmZ3SkkM', {
@@ -14,6 +7,12 @@ var osm = L.tileLayer('https://api.maptiler.com/maps/pastel/{z}/{x}/{y}.png?key=
 })
 osm.addTo(map);
 
-L.marker([35.75784, 51.54898]).addTo(map)
-    .bindPopup('50,000,000<br> 3,000,000')
-    .openPopup();
+//GeoJSON
+var pointsData = L.geoJSON(pointsJson, {
+    onEachFeature: function (feature, layer) {
+        layer.bindPopup(
+            '<b>Date: </b>' + feature.properties.date + '<br>' +
+            '<b>Yearly: </b>' + feature.properties.yearly + '<br>' +
+            '<b>Monthly: </b>' + feature.properties.monthly)
+    }
+}).addTo(map)
